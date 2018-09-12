72445061afbb0cfe6f7ab0d8395a53777983b6ed
smibbie <35998407+smibbie@users.noreply.github.com>
2018-09-12 17:01:37 -0400

    debugging

--- public/js/support.js
+++ public/js/support.js
@@ -25,7 +25,7 @@
 
   // Connect to socket.io
   // add "http://127.0.0.1:4000" to io.connect() arg if needed
-  const socket = io(`${window.location.hostname}:8000`, {});
+  const socket = io(window.location.hostname, {transports: [ 'websocket' ]});
 
   // Check for connection
   if (socket !== undefined) {
